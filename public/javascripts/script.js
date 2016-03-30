6fcbbf32ef0e6b6024e9e7b366b36a7c590b49c2
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2016-03-30 16:09:44 -0700

    Now really commit tracked files

---
+++ public/javascripts/script.js
@@ -0,0 +1,10 @@
+
+// http://angularscript.com/pure-angular-modal-modeless-dialog-directive-ngpopup/
+
+// home for ng-popup directive
+
+// https://github.com/MarkoCen/ngPopup
+
+// hiding columns solution
+
+// http://stackoverflow.com/questions/30334213/bootstrap-4-columns-2-hidden
